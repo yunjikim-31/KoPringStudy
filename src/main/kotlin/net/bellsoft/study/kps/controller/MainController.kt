01699b59f67215a0321b02845e6e7ffbee5743c4
Bell <bell04204@gmail.com>
2022-07-02 16:14:30 +0900

    feat: 메인 컨트롤러내 인덱스 엔드포인트 추가

---
+++ src/main/kotlin/net/bellsoft/study/kps/controller/MainController.kt
@@ -0,0 +1,14 @@
+package net.bellsoft.study.kps.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class MainController {
+
+    @GetMapping("/")
+    fun displayIndex(): String {
+        return "test response"
+    }
+
+}
