934dc35dc6ecd454ffc569b4841467282f6656be
Bell <bell04204@gmail.com>
2022-07-02 15:37:46 +0900

    Init kotlin + spring project

---
+++ src/main/kotlin/net/bellsoft/study/kps/KoPringStudyApplication.kt
@@ -0,0 +1,11 @@
+package net.bellsoft.study.kps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoPringStudyApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoPringStudyApplication>(*args)
+}
